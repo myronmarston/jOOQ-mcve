87770e849109eac229042973deda306a65325e69
Myron Marston <myron@squareup.com>
2019-08-09 16:07:15 -0700

    Demonstrate that you can't exclude a table-qualified column.
    For issue #9023.

--- src/main/resources/db/migration/V1__initialise_database.sql
+++ src/main/resources/db/migration/V1__initialise_database.sql
@@ -5,6 +5,15 @@ CREATE SCHEMA mcve;
 CREATE TABLE mcve.test (
   id    INT NOT NULL AUTO_INCREMENT,
   value INT,
+  some_other_value INT,
   
   CONSTRAINT pk_test PRIMARY KEY (id) 
+);
+
+CREATE TABLE mcve.test2 (
+  id    INT NOT NULL AUTO_INCREMENT,
+  value INT,
+  some_other_value INT,
+  
+  CONSTRAINT pk_test2 PRIMARY KEY (id) 
 );
